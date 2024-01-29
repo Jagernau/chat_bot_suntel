eb40ef57c40d11bb23a8da7bb4acd6adce465f27
jagernau <stepanenkov25@gmail.com>
2024-01-29 22:15:15 +0200

    рабочий вариант

--- buttons.py
+++ buttons.py
@@ -1,13 +1,15 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-
-keyboard = ReplyKeyboardMarkup(resize_keyboard=True )
-keyboard.add(
-    KeyboardButton(text="Клиенты exel"),
-    KeyboardButton(text="Дубли"),
+first_admin_menu = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True,keyboard=[
+    [
+        KeyboardButton(text="Клиенты exel"),
+        KeyboardButton(text="Дубли"),
+    ],
+    [
     KeyboardButton(text="Счётчик Сисем exel"),
-    KeyboardButton(text="Одинаковые имена объектов"),
     KeyboardButton(text="NewObject"),
+    ],
+]
 )
 
 
--- telegram.py
+++ telegram.py
@@ -1,96 +1,95 @@
+import asyncio
 import logging
-from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from aiogram.utils import executor
+import sys
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.enums import ParseMode
+from aiogram.filters import Command, CommandObject, CommandStart
+from aiogram.types import Message
+from aiogram.utils.markdown import hbold
+from aiogram import Router, F
+
 from database import get_data_from_database, get_data_from_object, get_klient_price, get_one_klient, get_klient_count, show_chenge, show_not_abons, show_select_date_chenge
 import config
 import funcs
-from buttons import keyboard
-from prettytable import PrettyTable
-import re
+from buttons import first_admin_menu
 
-API_TOKEN = config.TOKEN
 
-logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
-dp.middleware.setup(LoggingMiddleware())
 
-@dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
+TOKEN = config.TOKEN
+dp = Dispatcher()
+
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
     if str(message.from_user.id) in list(config.USERS_ID_LIST):
-        await message.reply("Привет!", reply_markup=keyboard)
+        await message.answer("Привет!", reply_markup=first_admin_menu)
     else:
-        await message.reply(f"Тебя нет в списке, твой id {message.from_user.id}. Напиши Максу свой id и своё Имя, если хочешь вступить.")
+        await message.answer(f"Тебя нет в списке, твой id {message.from_user.id}. Напиши Максу свой id и своё Имя, если хочешь вступить.")
 
-
-@dp.message_handler(content_types=["text"])
-async def send_data(message: types.Message):
-    if (
-        str(message.from_user.id) in config.USERS_ID_LIST
-    ):
-        if message.text == "Дубли":
+@dp.message(F.text == "Дубли")
+async def send_duble(message: Message) -> None:
+    if str(message.from_user.id) in list(config.USERS_ID_LIST):
             data = await get_data_from_database()
             text = ""
             for item in data:
                 text += f"{item[0]}: {item[1]}\n"
             await message.reply(text)
+    else:
+        pass
 
-        if message.text == "Клиенты exel":
+@dp.message(F.text == "Клиенты exel")
+async def send_clients(message: types.Message):
+    if str(message.from_user.id) in list(config.USERS_ID_LIST):
             get_klient_price()
-            file = types.InputFile(f'{funcs.get_yesterday()}_klient_price.xls')
-            await bot.send_document(chat_id=message.from_user.id, document=file)
+            file = types.input_file.FSInputFile(f'{funcs.get_yesterday()}_klient_price.xls')
+            await message.answer_document(document=file)
+    else:
+        pass
 
-
-        if  message.text != "Дубли" and message.text != "Клиенты exel" and message.text != "Счётчик Сисем exel":
-            data_array = get_one_klient(message.text)
-            if len(data_array) >= 1:
-                await message.reply(funcs.count_objects(data_array))
-            else:
-                message.reply(f"{message.text} Такого клиента нет")
-
-
-        if message.text != "Счётчик Сисем exel" and message.text != "Клиенты excel" and message.text != "Дубли":
-            data = await get_data_from_object(message.text)
-
-            text = ""
-            for item in data:
-                text += f"\n{item[0]}➡️{funcs.get_monitoring_system(str(item[2]))}➡️{item[3]}\n"
-            if text == "":
-                await message.reply(f"{message.text} не найден объект с таким именем")
-            else:
-                await message.reply(text)
-
-        if message.text == "Счётчик Сисем exel":
+@dp.message(F.text == "Счётчик Сисем exel")
+async def send_counter(message: types.Message):
+    if str(message.from_user.id) in list(config.USERS_ID_LIST):
             get_klient_count()
-            file = types.InputFile(f'{funcs.get_yesterday()}_klient_count.xls')
-            await bot.send_document(chat_id=message.from_user.id, document=file)
+            file = types.input_file.FSInputFile(f'{funcs.get_yesterday()}_klient_count.xls')
+            await message.answer_document(document=file)
+    else:
+        pass
 
-        if message.text == "Одинаковые имена объектов":
-            double = funcs.check_excel_double()
-            await message.reply(str(double))
-
-        if "NewObject" in message.text:
+@dp.message(F.text == "NewObject")
+async def send_new_object(message: types.Message):
+    if str(message.from_user.id) in list(config.USERS_ID_LIST):
             show_chenge()
-            file = types.InputFile(f'{funcs.get_yesterday()}_show_chenge_objects_to_day.xls')
-            await bot.send_document(chat_id=message.from_user.id, document=file)
-        
-        if "лишние:" in message.text:
-            show_not_abons(str(message.text).split(":")[1])
-            file = types.InputFile(f'{funcs.get_yesterday()}_difference.xls')
-            await bot.send_document(chat_id=message.from_user.id, document=file)
+            file = types.input_file.FSInputFile(f'{funcs.get_yesterday()}_show_chenge_objects_to_day.xls')
+            await message.answer_document(document=file)
+    else:
+        pass
 
-        if "new:" in message.text:
-            month_day = str(message.text).split(":")[1]
-            show_select_date_chenge(month_day)            
-            file = types.InputFile(f"{str(month_day)}_show_chenge_objects.xls")            
-            await bot.send_document(chat_id=message.from_user.id, document=file)
+@dp.message(Command("лишние"))
+async def command_outsiders(message: types.Message, command: CommandObject):
+    if command.args is None:
+        await message.answer(
+            "Ошибка: не переданы аргументы"
+        )
+        return
+    if str(message.from_user.id) in list(config.USERS_ID_LIST):
+            show_not_abons(command.args.split(" ")[0])
+            file = types.input_file.FSInputFile(f'{funcs.get_yesterday()}_difference.xls')
+            await message.answer_document(document=file)
+    else:
+        pass
 
+
+
+async def main() -> None:
+    # Initialize Bot instance with a default parse mode which will be passed to all API calls
+    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    # And the run events dispatching
+    await dp.start_polling(bot)
 
 
             
-
-if __name__ == '__main__':
-    from aiogram import executor
-    executor.start_polling(dp, skip_updates=True)
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
