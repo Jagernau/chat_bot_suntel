1af40876586b87bd6e5612e95466f545dbd73fc4
jagernau <stepanenkov25@gmail.com>
2024-01-30 21:39:02 +0200

    синхронный вариант

--- buttons.py
+++ buttons.py
@@ -1,15 +1,10 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
-first_admin_menu = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True,keyboard=[
-    [
-        KeyboardButton(text="Клиенты exel"),
-        KeyboardButton(text="Дубли"),
-    ],
-    [
-    KeyboardButton(text="Счётчик Сисем exel"),
-    KeyboardButton(text="NewObject"),
-    ],
-]
-)
+first_admin_menu = ReplyKeyboardMarkup(resize_keyboard=True)
+client_excel = KeyboardButton(text="Клиенты exel")
+dubles = KeyboardButton(text="Дубли")
+counter_excel = KeyboardButton(text="Счётчик Сисем exel")
+new_object = KeyboardButton(text="NewObject")
 
+first_admin_menu.add(client_excel, dubles, counter_excel, new_object)
 
--- telegram.py
+++ telegram.py
@@ -1,12 +1,4 @@
-import asyncio
-import logging
-import sys
-from aiogram import Bot, Dispatcher, Router, types
-from aiogram.enums import ParseMode
-from aiogram.filters import Command, CommandObject, CommandStart
-from aiogram.types import Message
-from aiogram.utils.markdown import hbold
-from aiogram import Router, F
+import telebot
 
 from database import get_data_from_database, get_data_from_object, get_klient_price, get_one_klient, get_klient_count, show_chenge, show_not_abons, show_select_date_chenge
 import config
@@ -17,80 +9,80 @@ from buttons import first_admin_menu
 
 
 TOKEN = config.TOKEN
-dp = Dispatcher()
+bot = telebot.TeleBot(TOKEN)
 
 
 
-@dp.message(CommandStart())
-def command_start_handler(message: Message) -> None:
+@bot.message_handler(commands=["start"])
+def command_start_handler(message: telebot.types.Message) -> None:
     if str(message.from_user.id) in list(config.USERS_ID_LIST):
-        return message.answer("Привет!", reply_markup=first_admin_menu)
+        bot.send_message(
+                message.chat.id, 
+                text=  "Привет!", 
+                reply_markup=first_admin_menu
+                )
     else:
-        return message.answer(f"Тебя нет в списке, твой id {message.from_user.id}. Напиши Максу свой id и своё Имя, если хочешь вступить.")
+        bot.send_message(message.chat.id, text=f"Тебя нет в списке, твой id {message.from_user.id}. Напиши Максу свой id и своё Имя, если хочешь вступить.")
 
-@dp.message(F.text == "Дубли")
-def send_duble(message: Message) -> None:
+@bot.message_handler(func=lambda message: message.text == "Дубли")
+def send_duble(message: telebot.types.Message) -> None:
     if str(message.from_user.id) in list(config.USERS_ID_LIST):
             data = get_data_from_database()
             text = ""
             for item in data:
                 text += f"{item[0]}: {item[1]}\n"
-            return message.reply(text)
+            bot.send_message(message.chat.id, text=text)
     else:
         pass
 
-@dp.message(F.text == "Клиенты exel")
-async def send_clients(message: types.Message):
+@bot.message_handler(func=lambda message: message.text == "Клиенты exel")
+def send_clients(message: telebot.types.Message):
     if str(message.from_user.id) in list(config.USERS_ID_LIST):
             get_klient_price()
-            file = types.input_file.FSInputFile(f'{funcs.get_yesterday()}_klient_price.xls')
+            file = telebot.types.InputFile(f'{funcs.get_yesterday()}_klient_price.xls')
 
-            await message.answer_document(document=file)
+            bot.send_document(message.chat.id, document=file)
     else:
         pass
 
-@dp.message(F.text == "Счётчик Сисем exel")
-def send_counter(message: types.Message):
+@bot.message_handler(func=lambda message: message.text == "Счётчик Сисем exel")
+def send_counter(message: telebot.types.Message):
     if str(message.from_user.id) in list(config.USERS_ID_LIST):
             get_klient_count()
-            file = types.input_file.FSInputFile(f'{funcs.get_yesterday()}_klient_count.xls')
-            return message.answer_document(document=file)
+            file = telebot.types.InputFile(f'{funcs.get_yesterday()}_klient_count.xls')
+            bot.send_document(message.chat.id, document=file)
     else:
         pass
 
-@dp.message(F.text == "NewObject")
-def send_new_object(message: types.Message):
+@bot.message_handler(func=lambda message: message.text == "NewObject")
+def send_new_object(message: telebot.types.Message):
     if str(message.from_user.id) in list(config.USERS_ID_LIST):
             show_chenge()
-            file = types.input_file.FSInputFile(f'{funcs.get_yesterday()}_show_chenge_objects_to_day.xls')
-            return message.answer_document(document=file)
+            file = telebot.types.InputFile(f'{funcs.get_yesterday()}_show_chenge_objects_to_day.xls')
+            bot.send_document(message.chat.id, document=file)
     else:
         pass
 
-@dp.message(Command("лишние"))
-def command_outsiders(message: types.Message, command: CommandObject):
-    if command.args is None:
-        return message.answer(
+@bot.message_handler(commands=["лишние"])
+def command_outsiders(message: telebot.types.Message, command: telebot.types.BotCommand) -> None:
+    if command.to_dict().get("args") is None:
+        bot.send_message(
+            message.chat.id,
+
             "Ошибка: не переданы аргументы"
         )
         return
     if str(message.from_user.id) in list(config.USERS_ID_LIST):
-            show_not_abons(command.args.split(" ")[0])
-            file = types.input_file.FSInputFile(f'{funcs.get_yesterday()}_difference.xls')
-            return message.answer_document(document=file)
+            show_not_abons(str(command.to_dict().get("args")).split(" ")[0])
+            file = telebot.types.InputFile(f'{funcs.get_yesterday()}_difference.xls')
+            bot.send_document(message.chat.id, document=file)
     else:
         pass
 
 
 
-def main() -> None:
-    # Initialize Bot instance with a default parse mode which will be passed to all API calls
-    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
-    # And the run events dispatching
-    return dp.start_polling(bot, timeout=500)
 
 
-            
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    asyncio.run(main())
+if __name__ == '__main__':
+    print('Бот запущен!')
+    bot.infinity_polling()            
